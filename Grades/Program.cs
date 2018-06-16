a4182982b9f239e0eaea48205259c9982895ec37
Matt Sharp <matthewssharp1@gmail.com>
2018-06-16 16:29:50 +0000

    Events and Delegates

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -12,17 +12,47 @@ namespace Grades
         {
 
             GradeBook book = new GradeBook();
+
+            //+= a delegate is multicasting it, so it can hit all methods that reference a name change
+            //we also don't have to type out new NameChangedDelegate(OnNameChanged)
+            //the compiler figures it out on its own
+            book.NameChanged += OnNameChanged;//tells delegate to call OnNameChanged whenever someone invokes this delegate
+            book.NameChanged += OnNameChanged2;
+
+            book.Name = "Danzig's Gradebook";
+            book.Name = "Guts' Gradebook";
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);//casting to int
+            WriteResult("Lowest", stats.LowestGrade);
 
             Console.Read();
 
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Gradebook changing name from {args.ExistingName} to {args.NewName}");
+        }
+
+        static void OnNameChanged2(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine("*********************");
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine($"{description}: {result}");
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result}");
         }
     }
 }
